5158053959fa06ba86db46917c78a3b750c4b504
eddyabban2 <eabban@ucsc.edu>
2026-03-03 19:24:33 -0800

    added stuff

--- test/test.py
+++ test/test.py
@@ -5,9 +5,46 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+# @cocotb.test()
+# async def test_project(dut):
+#     dut._log.info("Start")
 
+#     # Set the clock period to 10 us (100 KHz)
+#     clock = Clock(dut.clk, 10, unit="us")
+#     cocotb.start_soon(clock.start())
+
+#     # Reset
+#     dut._log.info("Reset")
+#     dut.ena.value = 1
+#     dut.ui_in.value = 0
+#     dut.uio_in.value = 0
+#     dut.rst_n.value = 0
+#     await ClockCycles(dut.clk, 10)
+#     dut.rst_n.value = 1
+
+#     dut._log.info("Test project behavior")
+
+#     # Set the input values you want to test
+#     dut.ui_in.value = 20
+#     dut.uio_in.value = 30
+
+#     # Wait for one clock cycle to see the output values
+#     await ClockCycles(dut.clk, 1)
+
+#     # The following assersion is just an example of how to check the output values.
+#     # Change it to match the actual expected output of your module:
+#     assert dut.uo_out.value == 50
+
+#     # Keep testing the module by changing the input values, waiting for
+#     # one or more clock cycles, and asserting the expected output values.
+
+RESET_STATE = 0
+READ_STATE = 1
+WRITE_STATE = 2
+COMPUTE_STATE = 3 
+IDLE_STATE = 5
 @cocotb.test()
-async def test_project(dut):
+async def write_test_1(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
@@ -20,21 +57,26 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.uio_in.value = 1
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert 50 == 51
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+
+# test reading and writing to each row 
+
+# test compute all 1 
+
+# test compute all 2 
+
+# test compute 
